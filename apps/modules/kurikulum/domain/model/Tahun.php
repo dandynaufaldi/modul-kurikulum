e012d822e896ac21359460cb3defc779e6ca9acc
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2019-12-03 00:12:34 +0700

    add Tahun domain
    Signed-off-by: Dandy Naufaldi <dandy.naufaldi@gmail.com>

---
+++ apps/modules/kurikulum/domain/model/Tahun.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Siakad\Kurikulum\Domain\Model;
+
+use DateTime;
+
+class Tahun
+{
+    private $tahun;
+
+    public function __construct(int $tahun)
+    {
+        $this->tahun = $tahun;
+    }
+
+    public function tahun()
+    {
+        return $this->tahun;
+    }
+
+    public function isGreater(Tahun $other) : bool
+    {
+        return $this->tahun > $other->tahun();
+    }
+
+    public function isEqual(Tahun $other) : bool
+    {
+        return $this->tahun === $other->tahun();
+    }
+}
---
+++ tests/kurikulum/TahunTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+use \PHPUnit\Framework\TestCase;
+use Siakad\Kurikulum\Domain\Model\Tahun;
+
+class TahunTest extends TestCase
+{
+    public function testCanBeInstantiated() : void
+    {
+        $tahun = new Tahun(2019);
+        $this->assertEquals(2019, $tahun->tahun());
+    }
+
+    public function testCompareEqual() : void
+    {
+        $tahun1 = new Tahun(2019);
+        $tahun2 = new Tahun(2019);
+        $tahun3 = new Tahun(2020);
+        
+        $this->assertTrue($tahun1->isEqual($tahun2));
+        $this->assertFalse($tahun2->isEqual($tahun3));
+    }
+
+    public function testCompareGreater() : void
+    {
+        $tahun1 = new Tahun(2019);
+        $tahun2 = new Tahun(2019);
+        $tahun3 = new Tahun(2020);
+
+        $this->assertTrue($tahun3->isGreater($tahun2));
+        $this->assertFalse($tahun1->isGreater($tahun2));
+    }
+}
