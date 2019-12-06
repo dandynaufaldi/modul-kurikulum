089b2f96ee9a4190fe2f1adb7b076d988921ef06
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2019-12-06 20:36:42 +0700

    service view form for creating new kurikulum
    Signed-off-by: Dandy Naufaldi <dandy.naufaldi@gmail.com>

---
+++ apps/modules/kurikulum/application/ViewFormCreateRMKResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Siakad\Kurikulum\Application;
+
+use Siakad\Kurikulum\Domain\Model\ProgramStudi;
+
+class ViewFormCreateRMKResponse
+{
+    public $listProgramStudi;
+
+    public function __construct()
+    {
+        $this->listProgramStudi = array();
+    }
+
+    public function addProgramStudi(ProgramStudi $programStudi)
+    {
+        $programStudiKV = [
+            'kode' => $programStudi->kode(),
+            'nama' => $programStudi->nama()->indonesia()
+        ];
+        $this->listProgramStudi[] = $programStudiKV;
+    }
+}
